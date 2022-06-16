f3eff98ce01ff48bab5c3bd0376e2a4d6b73d77d
최민한 <chmh0411@gmail.com>
2022-06-16 13:45:19 +0900

    add: countLabel

--- RxTest-Example/ViewController.swift
+++ RxTest-Example/ViewController.swift
@@ -6,15 +6,39 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+  
+  let countLabel: UILabel = {
+    let label = UILabel()
+    label.text = "0"
+    label.font = .boldSystemFont(ofSize: 25)
+    return label
+  }()
+  
+  let subtractButton: UIButton = {
+    let button = UIButton(type: .system)
+    button.setTitle("-", for: .normal)
+    return button
+  }()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .systemRed
+    setupLayout()
+  }
+}
+
+extension ViewController {
+  private func setupLayout() {
+    view.backgroundColor = .white
+    
+    view.addSubview(countLabel)
+    countLabel.snp.makeConstraints { make in
+      make.center.equalToSuperview()
     }
-
-
+  }
 }
 
