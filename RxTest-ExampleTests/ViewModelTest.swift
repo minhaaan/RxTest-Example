eeefc0c0943cbf12ae24c5e3a6a9a07471290f1e
최민한 <chmh0411@gmail.com>
2022-06-16 17:10:48 +0900

    implement: ViewModel Test

--- RxTest-ExampleTests/ViewModelTest.swift
+++ RxTest-ExampleTests/ViewModelTest.swift
@@ -9,8 +9,6 @@ import XCTest
 import RxSwift
 import RxRelay
 import RxTest
-import RxNimble
-import Nimble
 @testable import RxTest_Example
 
 class RxTest_ExampleTests: XCTestCase {
@@ -19,16 +17,25 @@ class RxTest_ExampleTests: XCTestCase {
   var viewModel: ViewModel!
   var disposeBag: DisposeBag!
   
+  var scheduler: TestScheduler!
+  var plusSubject: PublishSubject<Int>!
+  
   override func setUp() {
     viewController = ViewController()
     viewModel = ViewModel()
     disposeBag = DisposeBag()
+    
+    scheduler = TestScheduler(initialClock: 0)
+    plusSubject = PublishSubject<Int>()
   }
   
   override func tearDown() {
     viewController = nil
     viewModel = nil
     disposeBag = nil
+    
+    scheduler = nil
+    plusSubject = nil
   }
   
   func testCountLabelFirstLabelText() {
@@ -40,6 +47,43 @@ class RxTest_ExampleTests: XCTestCase {
     
     // Then
     XCTAssertEqual("0", countLabelText)
+  }
+  
+  func testButtonAction() {
+    let count = scheduler.createObserver(String.self)
+    
+    viewModel.countLabelText
+      .bind(to: count)
+      .disposed(by: disposeBag)
+    
+    scheduler.createColdObservable([
+      .next(10, ()),
+      .next(20, ()),
+      .next(30, ())
+    ]).bind(to: viewModel.addButtonTapped)
+      .disposed(by: disposeBag)
+    
+    scheduler.createColdObservable([
+      .next(40, ())
+    ]).bind(to: viewModel.resetButtonTapped)
+      .disposed(by: disposeBag)
+    
+    scheduler.createColdObservable([
+      .next(50, ())
+    ]).bind(to: viewModel.subtractButtonTapped)
+      .disposed(by: disposeBag)
+    
+    scheduler.start()
+    
+    XCTAssertEqual(count.events, [
+      .next(0, "0"),
+      .next(10, "1"),
+      .next(20, "2"),
+      .next(30, "3"),
+      .next(40, "0"),
+      .next(50, "-1")
+    ])
+    
   }
   
 }
