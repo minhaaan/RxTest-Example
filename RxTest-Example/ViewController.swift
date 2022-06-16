b520515e9a2c7aa5bc124514933e2b99ce5b07ad
최민한 <chmh0411@gmail.com>
2022-06-16 14:42:33 +0900

    implement ViewController

--- RxTest-Example/ViewController.swift
+++ RxTest-Example/ViewController.swift
@@ -6,9 +6,16 @@
 //
 
 import UIKit
+import RxSwift
+import RxRelay
+import RxCocoa
 import SnapKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+  
+  // MARK: Properties
+  
+  let viewModel = ViewModel()
   
   let countLabel: UILabel = {
     let label = UILabel()
@@ -19,25 +26,90 @@ class ViewController: UIViewController {
   
   let subtractButton: UIButton = {
     let button = UIButton(type: .system)
-    button.setTitle("-", for: .normal)
+    button.setImage(UIImage(systemName: "minus.square"), for: .normal)
+    button.tintColor = .systemBlue
     return button
   }()
+  
+  let addButton: UIButton = {
+    let button = UIButton(type: .system)
+    button.setImage(UIImage(systemName: "plus.square"), for: .normal)
+    button.tintColor = .systemBlue
+    return button
+  }()
+  
+  let resetButton: UIButton = {
+    let button = UIButton(type: .system)
+    button.setTitle("reset", for: .normal)
+    return button
+  }()
+  
+  let disposeBag = DisposeBag()
+  
+  // MARK: LifeCycle
   
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
     
+    bindData()
     setupLayout()
   }
 }
+
+// MARK: ViewModel bindData
+
+extension ViewController {
+  private func bindData() {
+    subtractButton.rx.tap
+      .bind(to: viewModel.subtractButtonTapped)
+      .disposed(by: disposeBag)
+    
+    addButton.rx.tap
+      .bind(to: viewModel.addButtonTapped)
+      .disposed(by: disposeBag)
+    
+    resetButton.rx.tap
+      .bind(to: viewModel.resetButtonTapped)
+      .disposed(by: disposeBag)
+    
+    viewModel.countLabelText
+      .distinctUntilChanged()
+      .bind(to: countLabel.rx.text)
+      .disposed(by: disposeBag)
+  }
+}
+
+// MARK: Layout
 
 extension ViewController {
   private func setupLayout() {
     view.backgroundColor = .white
     
-    view.addSubview(countLabel)
+    [
+      countLabel,
+      subtractButton,
+      addButton,
+      resetButton
+    ].forEach{ view.addSubview($0) }
+    
     countLabel.snp.makeConstraints { make in
       make.center.equalToSuperview()
+    }
+    
+    subtractButton.snp.makeConstraints { make in
+      make.trailing.equalTo(countLabel.snp.leading).offset(-20)
+      make.centerY.equalToSuperview()
+    }
+    
+    addButton.snp.makeConstraints { make in
+      make.leading.equalTo(countLabel.snp.trailing).offset(20)
+      make.centerY.equalToSuperview()
+    }
+    
+    resetButton.snp.makeConstraints { make in
+      make.top.equalTo(countLabel.snp.bottom).offset(20)
+      make.centerX.equalToSuperview()
     }
   }
 }
